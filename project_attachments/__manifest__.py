2a0e2d71616c114e4906d501b5cbf2112d4ad787
Ignacio J. Ortega <nacho.ortega@gmail.com>
2024-10-29 14:21:06 +0100

    [16.0][ADD] project_attachments: Recovers a functionality of previous version, adding a button to get all attached docs associated with a project and his tasks

---
+++ project_attachments/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Project attachments",
+    "version": "16.0.0.0.0",
+    "summary": "Adds a button to project view to visualize "
+    "the projects and project's task attachments",
+    "category": "Project",
+    "author": "TRIVAX SL,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/project",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
+    ],
+    "data": [
+        "views/project.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
